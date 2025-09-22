8f1a0ce09d1e1e8df1508e679e9175b171fbc515
Hrishikesh Rana <itsranahrishikesh@gmail.com>
2025-09-22 14:30:53 +0530

    init

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,66 @@
+import { Button } from "@repo/ui/button";
+import Image, { type ImageProps } from "next/image";
+
+type Props = Omit<ImageProps, "src"> & {
+  srcLight: string;
+  srcDark: string;
+};
+
+const ThemeImage = (props: Props) => {
+  const { srcLight, srcDark, ...rest } = props;
+
+  return (
+    <>
+      <Image {...rest} src={srcLight} className="imgLight" />
+      <Image {...rest} src={srcDark} className="imgDark" />
+    </>
+  );
+};
+
+export default function Home() {
+  return (
+    <div >
+      <main >
+        <ol>
+          <li>
+            Get started by editing <code>apps/web/app/page.tsx</code>
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+        <Button appName="web" >
+          Open alert
+        </Button>
+      </main>
+      <footer >
+        <a
+          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://turborepo.com?utm_source=create-turbo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to turborepo.com →
+        </a>
+      </footer>
+    </div>
+  );
+}
